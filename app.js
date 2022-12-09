5612c71272bc9a0aa5fed2b8841540301b321d2b
nicol29 <nicolascondrea@gmail.com>
2022-12-09 22:45:18 +0000

    Add icon images to each task element

--- app.js
+++ app.js
@@ -15,30 +15,42 @@ submitButton.addEventListener('click', () => {
 let addToStorage = (taskDesc) => {
     tasks.push({taskDesc});
     appendToDoc(taskDesc)
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 let appendToDoc = (taskDesc) => {
+    const editIcon = document.createElement('img')
+    const deleteIcon = document.createElement('img')
+    editIcon.src = './images/edit_icon.png';
+    deleteIcon.src = './images/delete_icon.png';
+
     let div = document.createElement('div');
     div.classList.add('task-description')
     toDoContent.appendChild(div)
     div.innerText = taskDesc;
+    div.appendChild(editIcon);
+    div.appendChild(deleteIcon);
+
     allTaskDivs += div;
 }
 
 for(let i = 0; i < tasks.length; i++){
+    const editIcon = document.createElement('img')
+    const deleteIcon = document.createElement('img')
+    editIcon.src = './images/edit_icon.png';
+    deleteIcon.src = './images/delete_icon.png';
+
     let div = document.createElement('div');
     div.classList.add('task-description')
     toDoContent.appendChild(div)
     div.innerText = tasks[i].taskDesc;
+    div.appendChild(editIcon);
+    div.appendChild(deleteIcon);
+
     if(i == tasks.length -1) allTaskDivs = document.querySelectorAll('.tasks div');
 }
 
 toDoContent.addEventListener('mouseover', (e) => {
-    let target = e.target;
-    console.log(target);
-    if(target.matches('div.task-description')) target.style.background = 'blue';
+    // if(e.target.matches('.task-description')) e.target.style.background = 'blue';
 });
-
-
-// localStorage.setItem('tasks', JSON.stringify(tasks));
