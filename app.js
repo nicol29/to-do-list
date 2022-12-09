4e67207b4c7ad1842d829b33126ae9502cbc7e6c
nicol29 <nicolascondrea@gmail.com>
2022-12-09 20:45:49 +0000

    Implement newly learned JS bubbling concept(event delegation)

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ let submitButton = document.querySelector('.add-button');
 let inputField = document.querySelector('.add-field');
 let toDoContent = document.querySelector('.tasks');
 let form = document.querySelector('form');
+let allTaskDivs;
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
 
 submitButton.addEventListener('click', () => {
     addToStorage(inputField.value);
@@ -18,14 +20,25 @@ let addToStorage = (taskDesc) => {
 
 let appendToDoc = (taskDesc) => {
     let div = document.createElement('div');
+    div.classList.add('task-description')
     toDoContent.appendChild(div)
     div.innerText = taskDesc;
+    allTaskDivs += div;
 }
 
 for(let i = 0; i < tasks.length; i++){
     let div = document.createElement('div');
+    div.classList.add('task-description')
     toDoContent.appendChild(div)
     div.innerText = tasks[i].taskDesc;
+    if(i == tasks.length -1) allTaskDivs = document.querySelectorAll('.tasks div');
 }
+
+toDoContent.addEventListener('mouseover', (e) => {
+    let target = e.target;
+    console.log(target);
+    if(target.matches('div.task-description')) target.style.background = 'blue';
+});
+
 
 // localStorage.setItem('tasks', JSON.stringify(tasks));
