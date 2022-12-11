2ad2987d38fc0f601aa48d9718780ef837996980
nicol29 <nicolascondrea@gmail.com>
2022-12-11 14:33:51 +0000

    Add functionality to edit tasks

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 let editIcon;
 let deleteIcon;
+let edit = false;
 
 
 let addToStorage = (taskDesc) => {
     tasks.push({taskDesc});
-    appendToDoc(taskDesc)
+    appendToDoc(taskDesc);
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
@@ -18,13 +19,17 @@ let addToStorage = (taskDesc) => {
 let appendToDoc = (taskDesc) => {
     let line = document.createElement('hr');
     let div = document.createElement('div');
+    let span = document.createElement('span');
 
-    div.classList.add('task-description')
-    toDoContent.appendChild(div)
-    div.innerText = taskDesc;
+    div.classList.add('task-description');
+    div.appendChild(span);
+    toDoContent.appendChild(div);
 
-    editIcon = document.createElement('img')
-    deleteIcon = document.createElement('img')
+    span.innerText = taskDesc;
+
+    editIcon = document.createElement('img');
+    deleteIcon = document.createElement('img');
+
 
     div.appendChild(editIcon);
     div.appendChild(deleteIcon);
@@ -32,6 +37,40 @@ let appendToDoc = (taskDesc) => {
 
     editIcon.src = './images/edit_icon.png';
     deleteIcon.src = './images/delete_icon.png';
+}
+
+let editTask = (whatTaskToEdit, secondChild) => {
+    let editInputField = document.createElement('input');
+    let finalizeChangesBtn = document.createElement('button');
+    let newSpan = document.createElement('span');
+    let correspondingTask;
+
+
+    for(let i = 0; i < tasks.length; i++){
+        correspondingTask = JSON.parse(localStorage.getItem('tasks'))[i].taskDesc;
+        if(correspondingTask == whatTaskToEdit.innerText) {
+
+            let tempInnerText = whatTaskToEdit.firstElementChild.innerText;
+
+            whatTaskToEdit.removeChild(whatTaskToEdit.firstElementChild);
+            whatTaskToEdit.insertBefore(finalizeChangesBtn, secondChild);
+            whatTaskToEdit.insertBefore(editInputField, finalizeChangesBtn);
+
+            editInputField.value = tempInnerText;
+            finalizeChangesBtn.innerText = 'Apply';
+
+            finalizeChangesBtn.addEventListener('click', () => {
+                tasks[i] = {taskDesc: `${editInputField.value}`};
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+
+                whatTaskToEdit.removeChild(editInputField);
+                whatTaskToEdit.removeChild(finalizeChangesBtn);
+                
+                whatTaskToEdit.insertBefore(newSpan, secondChild);
+                newSpan.innerText = tasks[i].taskDesc;
+            })
+        }
+    }
 }
 
 let deleteTask = (whatTaskToDelete) => {
@@ -52,10 +91,12 @@ let deleteTask = (whatTaskToDelete) => {
 for(let i = 0; i < tasks.length; i++){
     let div = document.createElement('div');
     let line = document.createElement('hr');
+    let span = document.createElement('span');
 
-    div.classList.add('task-description')
-    toDoContent.appendChild(div)
-    div.innerText = tasks[i].taskDesc;
+    div.classList.add('task-description');
+    div.appendChild(span);
+    toDoContent.appendChild(div);
+    span.innerText = tasks[i].taskDesc;
 
     editIcon = document.createElement('img')
     deleteIcon = document.createElement('img')
@@ -76,9 +117,9 @@ submitButton.addEventListener('click', () => {
 
 toDoContent.addEventListener('click', (e) => {
     if(e.target.matches('img[src*="edit"]')) {
-
+        editTask(e.target.parentNode, e.target);
     } else if(e.target.matches('img[src*="delete"]')){
         deleteTask(e.target.parentNode);
     }
-})
+});
 
